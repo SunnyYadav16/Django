b2b1e2fd13520b0d3eff97b077ac0b72237181f1
Sunny Yadav <sunny.y@simformsolutions.com>
2022-02-23 20:00:57 +0530

    New changes in effect

--- MyProject/food/urls.py
+++ MyProject/food/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index')
+    path("", views.index, name='index'),
+    path("detail-view/<int:item_id>/", views.detail_view, name='detail_view'),
 ]
