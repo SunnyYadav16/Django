9b86e428df5a1c73e38e827b718118f124f091d9
Sunny Yadav <sunny.y@simformsolutions.com>
2022-02-25 23:45:06 +0530

    Authorization started

--- MyProject/MyProject/urls.py
+++ MyProject/MyProject/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users.views import (
+    register,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include("food.urls")),
+    path('register/', register, name='register'),
 ]
