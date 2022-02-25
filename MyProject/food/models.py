a6b04b0ac7facf28f1cec7abaa8438a49b054bf8
Sunny Yadav <sunny.y@simformsolutions.com>
2022-02-25 17:36:27 +0530

    New Code Changes

--- MyProject/food/models.py
+++ MyProject/food/models.py
@@ -5,6 +5,7 @@ class Items(models.Model):
     item_name = models.CharField(max_length=120)
     item_desc = models.CharField(max_length=120)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="shorturl.at/gzR34")
 
     def __str__(self):
         return self.item_name
