4772daa362099ebc803171e9bb07c01deeb9d389
Sunny Yadav <sunny.y@simformsolutions.com>
2022-02-25 20:56:15 +0530

    New changes

--- MyProject/food/urls.py
+++ MyProject/food/urls.py
@@ -5,4 +5,7 @@ app_name = "food"
 urlpatterns = [
     path("", views.index, name='index'),
     path("detail-view/<int:item_id>/", views.detail_view, name='detail'),
+    path("add-item/", views.create_item, name='create_item'),
+    path("update-item/<int:item_id>/", views.update_item, name='update_item'),
+    path("delete-item/<int:item_id>/", views.delete_item, name='delete_item'),
 ]
