d29f3f5b7e8e167f6aee6cc9868d7b8398e2af29
Sunny Yadav <sunny.y@simformsolutions.com>
2022-02-28 20:01:39 +0530

    Code Completed for app

--- MyProject/food/urls.py
+++ MyProject/food/urls.py
@@ -3,9 +3,11 @@ from . import views
  
 app_name = "food"
 urlpatterns = [
-    path("", views.index, name='index'),
-    path("detail-view/<int:item_id>/", views.detail_view, name='detail'),
-    path("add-item/", views.create_item, name='create_item'),
+    # path("", views.index, name='index'),
+    path("", views.IndexClassView.as_view(), name='index'),
+    # path("detail-view/<int:item_id>/", views.detail_view, name='detail'),
+    path("detail-view/<int:pk>/", views.DetailClassView.as_view(), name='detail'),
+    path("add-item/", views.CreateItemView.as_view(), name='create_item'),
     path("update-item/<int:item_id>/", views.update_item, name='update_item'),
     path("delete-item/<int:item_id>/", views.delete_item, name='delete_item'),
 ]
